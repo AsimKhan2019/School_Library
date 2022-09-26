0c8521b47e7e003788fd7e8be3a0bcac709b6b8e
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-26 19:54:36 +0500

    Fixed linter errors

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -2,8 +2,8 @@ require_relative 'person'
 
 class Teacher < Person
   attr_reader :specialization
-  
-  def initialize(id,age, specialization, name = 'Unknown', parents_permission: true)
+
+  def initialize(id, age, specialization, name = 'Unknown', parents_permission: true)
     @specialization = specialization
     super(id, age, name, parents_permission)
     
