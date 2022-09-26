fa83965890afae56e718871f1423dded79dd39dc
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-09-26 19:03:55 +0500

    Fixed linter errors

--- classes/person.rb
+++ classes/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = "Unknown", parents_permission = true)
+  def initialize(age, name = 'Unknown', parents_permission : true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -11,15 +11,14 @@ class Person
 
   def is_of_age?(age)
     @age = age
-    return (@age >= 18) ? true : false
+    @age >= 18 ? true : false
   end
 
   private :is_of_age?
 
-  public 
-  def can_use_service?(age, parents_permission)
+  public def can_use_service?(age, parents_permission)
     @age = age
     @parents_permission = parents_permission
-    return (@age >= 18 || @parents_permission === true) ? true : false
+    @age >= 18 || @parents_permission == true ? true : false
   end
 end
--- classes/teacher.rb
+++ classes/teacher.rb
@@ -3,12 +3,12 @@ require_relative 'person'
 class Teacher < Person
   attr_reader :specializtion
 
-  def initialize(id,name = "Unknown", parents_permission = true, age, specializtion)
+  def initialize(id,name = 'Unknown', parents_permission : true, age, specializtion)
     super(id, name, parents_permission, age)
     @specializtion = specializtion
   end
 
   def can_use_service?
-    return true
+    true
   end
 end
